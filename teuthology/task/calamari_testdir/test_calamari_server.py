b0a49f16d07a225a0537ce8e1bfd360b30074b3b
Dan Mick <dan.mick@inktank.com>
2014-02-01 00:59:24 -0800

    update version of test_calamari_server.py to latest

--- teuthology/task/calamari_testdir/test_calamari_server.py
+++ teuthology/task/calamari_testdir/test_calamari_server.py
@@ -1,49 +1,71 @@
 #!/usr/bin/env python
 
-import argparse
 import datetime
-import sys
-import json
+import os
+import logging
+import logging.handlers
 import uuid
 import unittest
 from http_client import AuthenticatedHttpClient
 
+log = logging.getLogger(__name__)
+log.addHandler(logging.StreamHandler())
+log.setLevel(logging.INFO)
+
+global base_uri
 global client
+base_uri = None
+client = None
+
+def setUpModule():
+    global base_uri
+    global client
+    try:
+        base_uri = os.environ['CALAMARI_BASE_URI']
+    except KeyError:
+        log.error('Must define CALAMARI_BASE_URI')
+        os._exit(1)
+    if not base_uri.endswith('/'):
+        base_uri += '/'
+    if not base_uri.endswith('api/v1/'):
+        base_uri += 'api/v1/'
+    client = AuthenticatedHttpClient(base_uri, 'admin', 'admin')
+    client.login()
 
 class RestTest(unittest.TestCase):
     'Base class for all tests here; get class\'s data'
 
     def setUp(self):
+        # Called once for each test_* case.  A bit wasteful, but we
+        # really like using the simple class variable self.uri
+        # to customize each derived TestCase
         method = getattr(self, 'method', 'GET')
-        self.response = get_object(method, self.uri)
+        self.response = self.get_object(method, self.uri)
 
-    def tearDown(self):
-        self.response = None
+    def get_object(self, method, url):
+        'Return Python object decoded from JSON response to method/url'
+        return client.request(method, url).json()
 
 class TestUserMe(RestTest):
 
-    def __init__(self, test_name):
-        self.uri = 'user/me'
-        super(self.__class__, self).__init__(test_name)
+    uri = 'user/me'
 
     def test_me(self):
         self.assertEqual(self.response['username'], 'admin')
 
 class TestCluster(RestTest):
 
-    def __init__(self, test_name):
-        self.uri = 'cluster'
-        super(self.__class__, self).__init__(test_name)
+    uri = 'cluster'
 
     def test_id(self):
         self.assertEqual(self.response[0]['id'], 1)
 
     def test_times(self):
-        for t in (
+        for time in (
             self.response[0]['cluster_update_time'],
             self.response[0]['cluster_update_attempt_time'],
         ):
-            self.assertTrue(is_datetime(t))
+            self.assertTrue(is_datetime(time))
 
     def test_api_base_url(self):
         api_base_url = self.response[0]['api_base_url']
@@ -52,19 +74,17 @@ class TestCluster(RestTest):
 
 class TestHealth(RestTest):
 
-    def __init__(self, test_name):
-        self.uri = 'cluster/1/health'
-        super(self.__class__, self).__init__(test_name)
+    uri = 'cluster/1/health'
 
     def test_cluster(self):
         self.assertEqual(self.response['cluster'], 1)
 
     def test_times(self):
-        for t in (
+        for time in (
             self.response['cluster_update_time'],
             self.response['added'],
         ):
-            self.assertTrue(is_datetime(t))
+            self.assertTrue(is_datetime(time))
 
     def test_report_and_overall_status(self):
         self.assertIn('report', self.response)
@@ -72,9 +92,7 @@ class TestHealth(RestTest):
 
 class TestHealthCounters(RestTest):
 
-    def __init__(self, test_name):
-        self.uri = 'cluster/1/health_counters'
-        super(self.__class__, self).__init__(test_name)
+    uri = 'cluster/1/health_counters'
 
     def test_cluster(self):
         self.assertEqual(self.response['cluster'], 1)
@@ -98,19 +116,17 @@ class TestHealthCounters(RestTest):
 
 class TestSpace(RestTest):
 
-    def __init__(self, test_name):
-        self.uri = 'cluster/1/space'
-        super(self.__class__, self).__init__(test_name)
+    uri = 'cluster/1/space'
 
     def test_cluster(self):
         self.assertEqual(self.response['cluster'], 1)
 
     def test_times(self):
-        for t in (
+        for time in (
             self.response['cluster_update_time'],
             self.response['added'],
         ):
-            self.assertTrue(is_datetime(t))
+            self.assertTrue(is_datetime(time))
 
     def test_space(self):
         for size in ('free_bytes', 'used_bytes', 'capacity_bytes'):
@@ -123,76 +139,69 @@ class TestSpace(RestTest):
             self.assertGreater(self.response['report'][size], 0)
 
 class TestOSD(RestTest):
-    def __init__(self, test_name):
-        self.uri = 'cluster/1/osd'
-        super(self.__class__, self).__init__(test_name)
+
+    uri = 'cluster/1/osd'
 
     def test_cluster(self):
         self.assertEqual(self.response['cluster'], 1)
 
     def test_times(self):
-        for t in (
+        for time in (
             self.response['cluster_update_time'],
             self.response['added'],
         ):
-            self.assertTrue(is_datetime(t))
-
-    def test_number_of_osds(self):
-        self.assertEqual(len(self.response['osds']), 2)
+            self.assertTrue(is_datetime(time))
 
     def test_osd_uuid(self):
-        for o in self.response['osds']:
-            uuidobj = uuid.UUID(o['uuid'])
-            self.assertEqual(str(uuidobj), o['uuid'])
+        for osd in self.response['osds']:
+            uuidobj = uuid.UUID(osd['uuid'])
+            self.assertEqual(str(uuidobj), osd['uuid'])
 
     def test_osd_pools(self):
-        for o in self.response['osds']:
-            self.assertIsInstance(o['pools'], list)
-            self.assertIsInstance(o['pools'][0], basestring)
+        for osd in self.response['osds']:
+            self.assertIsInstance(osd['pools'], list)
+            self.assertIsInstance(osd['pools'][0], basestring)
 
     def test_osd_up_in(self):
-        for o in self.response['osds']:
+        for osd in self.response['osds']:
             for flag in ('up', 'in'):
-                self.assertIn(o[flag], (0, 1))
+                self.assertIn(osd[flag], (0, 1))
 
     def test_osd_0(self):
-        osd0 = get_object('GET', 'cluster/1/osd/0')['osd']
+        osd0 = self.get_object('GET', 'cluster/1/osd/0')['osd']
         for field in osd0.keys():
             if not field.startswith('cluster_update_time'):
                 self.assertEqual(self.response['osds'][0][field], osd0[field])
 
 class TestPool(RestTest):
-    def __init__(self, test_name):
-        self.uri = 'cluster/1/pool'
-        super(self.__class__, self).__init__(test_name)
+
+    uri = 'cluster/1/pool'
 
     def test_cluster(self):
-        for p in self.response:
-            self.assertEqual(p['cluster'], 1)
+        for pool in self.response:
+            self.assertEqual(pool['cluster'], 1)
 
     def test_fields_are_ints(self):
-        for p in self.response:
+        for pool in self.response:
             for field in ('id', 'used_objects', 'used_bytes'):
-                self.assertIsInstance(p[field], int)
+                self.assertIsInstance(pool[field], int)
 
     def test_name_is_str(self):
-        for p in self.response:
-            self.assertIsInstance(p['name'], basestring)
+        for pool in self.response:
+            self.assertIsInstance(pool['name'], basestring)
 
     def test_pool_0(self):
         poolid = self.response[0]['id']
-        pool = get_object('GET', 'cluster/1/pool/{id}'.format(id=poolid))
+        pool = self.get_object('GET', 'cluster/1/pool/{id}'.format(id=poolid))
         self.assertEqual(self.response[0], pool)
 
 class TestServer(RestTest):
 
-    def __init__(self, test_name):
-        self.uri = 'cluster/1/server'
-        super(self.__class__, self).__init__(test_name)
+    uri = 'cluster/1/server'
 
     def test_ipaddr(self):
-        for s in self.response:
-            octets = s['addr'].split('.')
+        for server in self.response:
+            octets = server['addr'].split('.')
             self.assertEqual(len(octets), 4)
             for octetstr in octets:
                 octet = int(octetstr)
@@ -201,14 +210,14 @@ class TestServer(RestTest):
                 self.assertLessEqual(octet, 255)
 
     def test_hostname_name_strings(self):
-        for s in self.response:
+        for server in self.response:
             for field in ('name', 'hostname'):
-                self.assertIsInstance(s[field], basestring)
+                self.assertIsInstance(server[field], basestring)
 
     def test_services(self):
-        for s in self.response:
-            self.assertIsInstance(s['services'], list)
-            for service in s['services']:
+        for server in self.response:
+            self.assertIsInstance(server['services'], list)
+            for service in server['services']:
                 self.assertIn(service['type'], ('osd', 'mon', 'mds'))
 
 #
@@ -217,33 +226,9 @@ class TestServer(RestTest):
 
 DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
 
-def is_datetime(t):
-    datetime.datetime.strptime(t, DATETIME_FORMAT)
+def is_datetime(time):
+    datetime.datetime.strptime(time, DATETIME_FORMAT)
     return True
 
-def get_object(method, url):
-    'Return Python object decoded from JSON response to method/url'
-    return client.request(method, url).json()
-
-def parse_args():
-    p = argparse.ArgumentParser('Test Calamari server')
-    p.add_argument(
-        '-u', '--uri',
-        required=True,
-        help='URI of REST server to test'
-    )
-    return p.parse_known_args()
-
 if __name__ == '__main__':
-    cmdargs, unittest_args = parse_args()
-    uri = cmdargs.uri
-    if not uri.endswith('/'):
-        uri += '/'
-    if not uri.endswith('api/v1/'):
-        uri += 'api/v1/'
-    client = AuthenticatedHttpClient(uri, 'admin', 'admin')
-    client.login()
-
-    sys.argv = [sys.argv[0]]
-    sys.argv.extend(unittest_args)
     unittest.main()
