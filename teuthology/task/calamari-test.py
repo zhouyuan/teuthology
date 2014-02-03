f9ad3d3cd74f043c3578c9e099f273e03ecfd443
Dan Mick <dan.mick@inktank.com>
2014-02-03 10:56:30 -0800

    calamari-test: add logging and delay
    Signed-off-by: Dan Mick <dan.mick@inktank.com>

--- teuthology/task/calamari-test.py
+++ teuthology/task/calamari-test.py
@@ -1,20 +1,32 @@
 """
 Run the calamari smoketest
 """
+import logging
 import os
 import subprocess
+import time
+
+log = logging.getLogger(__name__)
+
 def task(ctx, config):
     """
     Tests to run are in calamari_testdir.
+    delay: wait this long before starting
  
         tasks:
         - calamari-test:
+            delay: 30
             server: server.0
     """
+    delay = config.get('delay', 0)
+    if delay:
+        log.info("delaying %d sec", delay)
+        time.sleep(delay)
     testhost = ctx.cluster.only(config['server']).remotes.keys()[0].name
     testhost = testhost.split('@')[1]
     mypath = os.path.dirname(__file__)
     cmd_list = [os.path.join(mypath, 'calamari_testdir',
                              'test_calamari_server.py')]
     os.environ['CALAMARI_BASE_URI'] = 'http://{0}/api/v1/'.format(testhost)
+    log.info("testing %s", testhost)
     return subprocess.call(cmd_list)
